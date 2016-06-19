1f4019cbb3d8adf0ecbf24e95c4482f8bf6eee62
lightofdavinci <myrovoy@mail.ru>
2016-06-19 16:09:41 -0400

    prepare to deploy

---
+++ server.js
@@ -0,0 +1,17 @@
+'use strict';
+var express = require('express');
+var app = express();
+
+app.use('/css', express.static(process.cwd() + '/css'));
+app.use('/fonts', express.static(process.cwd() + 'fonts'));
+app.use('/js', express.static(process.cwd() + '/js'));
+app.use('/views', express.static(process.cwd() + 'views'));
+
+app.get('/',function (req, res) {
+			res.sendFile(process.cwd() + '/index.html');
+		});
+
+var port = process.env.PORT;
+app.listen(port,  function () {
+	console.log('Node.js listening on port ' + port + '...');
+});
